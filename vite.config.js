b58259387ea78672742ddcd05a066e025bfcb48f
hieurury <hieurury007@gmail.com>
2025-10-22 10:04:20 +0700

    chuẩn bị deloy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
